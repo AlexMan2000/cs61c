2979ac7884160dc70aa0737bb920640593acdae7
Jiasheng Ni <alexman@Jiashengs-MacBook-Air.local>
2023-09-19 19:10:49 -0400

    20230919

--- sp23/sp23-lab-starter-main/lab03/ex4_discrete_fn.s
+++ sp23/sp23-lab-starter-main/lab03/ex4_discrete_fn.s
@@ -6,6 +6,15 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    # Initialize the registers
+    addi t0 x0 6
+    addi t1 x0 61
+    addi t2 x0 17
+    addi t3 x0 -38
+    addi t4 x0 19
+    addi t5 x0 42
+    addi t6 x0 5
+    
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
